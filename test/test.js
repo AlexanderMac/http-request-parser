034baec5480de72def59819b5543a5ee2eb925cb
AlexanderMac <amatsibarov@gmail.com>
2014-12-14 21:17:51 +0700

    Fix tests

--- test/test.js
+++ test/test.js
@@ -9,7 +9,7 @@ var should = require('should');
 
 describe('#parse()', function() {
   
-  describe("get method", function() {    
+  describe("GET method", function() {    
     it('should parse valid http request message', function() {    
       var request = [
         'GET http://localhost/test HTTP/2.1',
@@ -57,9 +57,67 @@ describe('#parse()', function() {
       var actual = parser.parse(request);
       actual.should.eql(expected);
     }); 
+    
+    it('should parse valid http request message with contentType=application/x-www-form-urlencoded', function() {    
+      var request = [
+        'GET https://localhost/test?dd=e HTTP/2.0',
+        'Host: localhost',
+        'Connection: keep-alive',      
+        'Cache-Control: no-cache',
+        'User-Agent: Mozilla/5.0 (Windows NT 6.1 WOW64)',
+        'Content-Type: application/x-www-form-urlencoded; charset=UTF-8',
+        'Content-Length: 301',
+        'Accept: */*',
+        'Accept-Encoding: gzip,deflate',
+        'Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
+        '',
+        'id=11&message=Hello'
+      ].join('\n');
+    
+      var expected = { 
+        method: 'GET',
+        protocol: 'HTTPS',
+        url: 'localhost/test?dd=e',
+        protocolVersion: 'HTTP/2.0',
+        host: 'localhost',
+        headers: [ 
+          { name: 'Connection', values: [ { value: 'keep-alive', params: null } ] },          
+          { name: 'Cache-Control', values: [ { value: 'no-cache', params: null } ] },
+          { name: 'User-Agent', values: [ 
+            { value: 'Mozilla/5.0 (Windows NT 6.1 WOW64)', params: null } 
+          ]},
+          { name: 'Content-Type', values: [ { value: 'application/x-www-form-urlencoded', params: 'charset=UTF-8' } ] },
+          { name: 'Content-Length', values: [ { value: '301', params: null } ] },
+          { name: 'Accept', values: [ { value: '*/*', params: null } ] },
+          { name: 'Accept-Encoding', values: [ 
+            { value: 'gzip', params: null },
+            { value: 'deflate', params: null }
+          ]},
+          { name: 'Accept-Language', values: [ 
+            { value: 'ru-RU', params: null },
+            { value: 'ru', params: 'q=0.8' },
+            { value: 'en-US', params: 'q=0.6' },
+            { value: 'en', params: 'q=0.4' } 
+          ]}
+        ],
+        cookies: null,
+        body: { 
+          hasBody: true,
+          contentType: 'application/x-www-form-urlencoded',
+          boundary: null,
+          formDataParams: [
+            { name: "id", value: "11" },
+            { name: "message", value: "Hello" }
+          ] 
+        } 
+      };
+    
+      var actual = parser.parse(request);
+      actual.should.eql(expected);
+    });
   });
   
-  describe("post method", function() {      
+  describe("POST method", function() {      
     it('should parse valid http request message with contentType=multipart/form-data', function() {    
       var request = [
         'POST http://localhost/test HTTP/2.1',
@@ -120,64 +178,6 @@ describe('#parse()', function() {
           formDataParams: [
             { name: "Name", value: "Ivanov" },
             { name: "Age", value: "25" }
-          ] 
-        } 
-      };
-    
-      var actual = parser.parse(request);
-      actual.should.eql(expected);
-    });
-    
-    it('should parse valid http request message with contentType=application/x-www-form-urlencoded', function() {    
-      var request = [
-        'POST https://localhost/test?dd=e HTTP/2.0',
-        'Host: localhost',
-        'Connection: keep-alive',      
-        'Cache-Control: no-cache',
-        'User-Agent: Mozilla/5.0 (Windows NT 6.1 WOW64)',
-        'Content-Type: application/x-www-form-urlencoded; charset=UTF-8',
-        'Content-Length: 301',
-        'Accept: */*',
-        'Accept-Encoding: gzip,deflate',
-        'Accept-Language: ru-RU,ru;q=0.8,en-US;q=0.6,en;q=0.4',
-        '',
-        'id=11&message=Hello'
-      ].join('\n');
-    
-      var expected = { 
-        method: 'POST',
-        protocol: 'HTTPS',
-        url: 'localhost/test?dd=e',
-        protocolVersion: 'HTTP/2.0',
-        host: 'localhost',
-        headers: [ 
-          { name: 'Connection', values: [ { value: 'keep-alive', params: null } ] },          
-          { name: 'Cache-Control', values: [ { value: 'no-cache', params: null } ] },
-          { name: 'User-Agent', values: [ 
-            { value: 'Mozilla/5.0 (Windows NT 6.1 WOW64)', params: null } 
-          ]},
-          { name: 'Content-Type', values: [ { value: 'application/x-www-form-urlencoded', params: 'charset=UTF-8' } ] },
-          { name: 'Content-Length', values: [ { value: '301', params: null } ] },
-          { name: 'Accept', values: [ { value: '*/*', params: null } ] },
-          { name: 'Accept-Encoding', values: [ 
-            { value: 'gzip', params: null },
-            { value: 'deflate', params: null }
-          ]},
-          { name: 'Accept-Language', values: [ 
-            { value: 'ru-RU', params: null },
-            { value: 'ru', params: 'q=0.8' },
-            { value: 'en-US', params: 'q=0.6' },
-            { value: 'en', params: 'q=0.4' } 
-          ]}
-        ],
-        cookies: null,
-        body: { 
-          hasBody: true,
-          contentType: 'application/x-www-form-urlencoded',
-          boundary: null,
-          formDataParams: [
-            { name: "id", value: "11" },
-            { name: "message", value: "Hello" }
           ] 
         } 
       };
